5797c478e1d97e49a788b1368bd8aff431471699
John Kadyszewski <john@kadyszewski.com>
2017-02-07 08:32:01 -0500

    .

--- public/js/brambles.js
+++ public/js/brambles.js
@@ -183,8 +183,8 @@
     GameView.prototype.initialize = function() {
         this.canvas.width = this.options.width;
         this.canvas.height = this.options.height;
-        var map1 = new GameView.map(this.options)
-        map1.generate();
+        new GameView.map(this.options);
+        map.generate();
         objects.forEach(function(object)
         {
           if (object.id == socketId)
